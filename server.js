16f33f8ad372fc1ab0417e254988702bf3e06584
ohvanadiumjam <ohvanadiumjam@gmail.com>
2025-06-21 16:35:44 +0900

    fixed security issues 2

--- server.js
+++ server.js
@@ -60,32 +60,16 @@ app.post('/upload', (req, res) => {
 
 // 파일 다운로드 엔드포인트
 app.get('/download/:code', (req, res) => {
-    const code = req.params.code;
-    const fileData = files[code];
+    let code = req.params.code;
+    let fileData = files[code];
 
     if (!fileData) {
         return res.status(404).send('Invalid code or file not found.');
     }
 
     res.download(fileData.path, fileData.uuidName, (err) => {
-        if (!err) {
-            fileData.downloaded = true; // 다운로드 기록
-            delete files[code];         // 코드 삭제는 유지
-        }
+        if (!err) delete files[code]; // 다운로드 후 코드 삭제
     });
-});
-
-
-// 파일 상태 확인용 엔드포인트
-app.get('/status/:code', (req, res) => {
-    const code = req.params.code;
-    const fileData = files[code];
-
-    if (!fileData) {
-        return res.status(404).json({ exists: false });
-    }
-
-    res.json({ exists: true, downloaded: !!fileData.downloaded });
 });
 
 
